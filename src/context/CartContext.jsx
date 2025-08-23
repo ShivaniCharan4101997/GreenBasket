fc4eccaa4a0cba16c3edbcb514ece70a3744b12b
shivani charan <charanshivani8@gmail.com>
2025-08-23 21:29:19 +0530

    update cart state

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext();
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState();
+  const [cart, setCart] = useState([]);
 
   function addToCart(product) {
     setCart((prevCart) => {
